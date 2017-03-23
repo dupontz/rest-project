a38d01f048437bf5f338883dd702b0d97a05f004
dupont <gustavo.dupont@gmail.com>
2017-03-23 17:30:56 -0300

    model feira gerado apartir do model

--- feiraproject/urls.py
+++ feiraproject/urls.py
@@ -1,4 +1,4 @@
-"""restproject URL Configuration
+"""feiraproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
